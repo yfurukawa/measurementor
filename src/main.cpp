0b8d9d644deaa74a4ad23a9d89cb43f4ff5ad935
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-02 16:34:27 +0900

    OpenProjectから情報を取得することができるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include "pts/OpenProjectFactory.h"
+#include "pts/OpenProject.h"
+
 int main( int argc, char* argv[] )
 {
+	pts::OpenProjectFactory::getInstance()->createPts()->collectAllActiveProject();
 	return 0;
 }
