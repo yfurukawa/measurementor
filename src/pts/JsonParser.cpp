f23742a94221a8370bd7329f9b83751b91aa807a
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-04 16:20:28 +0900

    取得したJson文字列からプロジェクトリストを作成する処理をついかした

--- src/pts/JsonParser.cpp
+++ src/pts/JsonParser.cpp
@@ -1,13 +1,20 @@
 #include "JsonParser.h"
 #include "nlohmann/json.hpp"
 #include "../domain/Project.h"
+#include "../domain/domainPrimitives/MeasurementPrimitives.h"
 
 namespace pts
 {
 
-void JsonParser::collectProjectData( std::string jsonString, std::list<measurementor::Project> projectList )
+void JsonParser::collectProjectData( const std::string jsonString, std::list<std::shared_ptr<measurementor::Project>>& projectList )
 {
-
+    auto j = nlohmann::json::parse( jsonString );
+    
+    for( int count = 0; count < j["count"]; ++count ) {
+        measurementor::Id id(j["_embedded"]["elements"][count]["id"]);
+        measurementor::Name name(j["_embedded"]["elements"][count]["name"]);
+        projectList.push_back( std::make_shared<measurementor::Project>(id, name) );
+    }
 }
 
 }
