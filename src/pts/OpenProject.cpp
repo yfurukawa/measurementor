e2adf4ed9288bbe840ac0cf3f3da9b2784b1a049
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-05 22:08:38 +0900

    OpenProjectコンテナが壊れたため作り直したことに伴うtokenの変更

--- src/pts/OpenProject.cpp
+++ src/pts/OpenProject.cpp
@@ -16,7 +16,7 @@ OpenProject::OpenProject(std::unique_ptr<::TcpClient> tcpClient)
 void OpenProject::collectAllActiveProject( std::map<unsigned int,std::shared_ptr<measurementor::Project>>& projectList )
 {
     // TODO tokenはファイルから読み込むようにする
-    std::string key(createBasicAuthorizationKey("apikey:46bd7a8446944b9da08c84c26cc14188c88d894865e4475de8c293d4ca9a94ce"));
+    std::string key(createBasicAuthorizationKey("apikey:d54087631332a33b932b200c3d49496efdc7fb156b2405b2baf6fece4018e9f0"));
     std::string message("GET /api/v3/queries/available_projects HTTP/1.1\r\nHost:localhost\r\nAuthorization: Basic " + key + "\r\n\r\n");
     
     tcpClient_->sendData(message);
