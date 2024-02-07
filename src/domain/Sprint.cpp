15b0ecb8d8d9b23aafa33d5ffb0bdb35ce959ec7
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-07 20:44:00 +0900

    スプリントを表すクラスを追加した

---
+++ src/domain/Sprint.cpp
@@ -0,0 +1,54 @@
+#include "Item.h"
+#include "Sprint.h"
+#include "Task.h"
+#include <iostream>
+
+namespace measurementor
+{
+
+Sprint::Sprint( Id id, Name name )
+    : id_(id),
+    name_(name),
+    projectId_(0),
+    totalPoint_(0),
+    status_("")
+{
+    items_.clear();
+}
+
+void Sprint::assignProject( ProjectId projectId )
+{
+    projectId_ = ProjectId{ projectId.get() };
+}
+
+void Sprint::addItem( Id itemId, std::unique_ptr<Item> item )
+{
+    items_.insert( std::make_pair(itemId, std::move(item) ) );
+}
+
+std::optional<ProjectId> Sprint::projectId() const
+{
+    if( projectId_ == 0 )
+    {
+        return std::nullopt;
+    }
+    return projectId_;
+}
+
+void Sprint::aggrigateStoryPoint()
+{
+    for( auto item = begin(items_); item != end(items_); ++item )
+    {
+        totalPoint_ = totalPoint_ + item->second->point();
+    }
+}
+
+void Sprint::printChild()
+{
+    for( auto item = begin(items_); item != end(items_); ++item )
+    {
+        std::cout << item->second->id() << " : " << item->second->name() << std::endl;
+    }
+}
+
+}
---
+++ src/domain/Sprint.h
@@ -0,0 +1,92 @@
+/*!
+ @file      Sprint.h
+ @brief     スプリントデータ
+*/
+#pragma once
+
+// ---------------< include >----------------------------
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include "domainPrimitives/MeasurementPrimitives.h"
+
+// --------------< namespace >---------------------------
+namespace measurementor
+{
+
+// ---------< forward declaration >----------------------
+class Item;
+
+/*!
+ @class     Sprint
+ @brief     スプリントデータ
+ @note      本クラスは、Project Tracking System (PTS) から取得したバージョンの情報の内、スプリントに相当するデータを保持するものである。<br>
+ OpenProjectでは、ある期間を表すのにVersionを用いるが直感的ではないので、SCRUMの用語であるSprintを用いることとする。
+*/
+class Sprint final
+{
+private:
+    /*!
+     @brief  デフォルトコンストラクタ
+    */
+    Sprint() = delete;
+
+public:
+    /*!
+     @brief  コンストラクタ
+    */
+    Sprint( Id id, Name name );
+
+    /*!
+     @brief  デフォルトデストラクタ
+    */
+    virtual ~Sprint() = default;
+
+    /*!
+     @brief      SprintをProjectに割り当てる
+     @param[in]  id 割当先ProjectのID
+    */
+    void assignProject( ProjectId projectId );
+
+    /*!
+     @brief      Sprintにアイテムを追加する
+     @param[in]  id アイテムのID
+    */
+    void addItem( Id itemId, std::unique_ptr<Item> item );
+
+    /*!
+     @brief      親となるプロジェクトのIDを返す
+     @return     プロジェクトのID
+    */
+    std::optional<ProjectId> projectId() const;
+
+    /*!
+     @brief      割り当てられたアイテムの有無を返す
+     @return     true : アイテムあり
+    */
+    bool hasChild() const
+    {
+        return !items_.empty();
+    }
+
+    /*!
+     @brief      アイテムのストーリーポイントを集計する
+    */
+    void aggrigateStoryPoint();
+
+    Id id() { return id_; };
+    Name name() { return name_; };
+
+    void printChild();
+    
+private:
+    const Id id_;                                  //!< Sprint ID
+    const Name name_;                              //!< Sprint名称
+    ProjectId projectId_;                          //!< 親のID
+    Point totalPoint_;                             //!< プロジェクトの総見積もりポイント
+    std::string status_;                           //!< スプリントのステータス（未完了open/完了closeを表す）
+    std::map<Id, std::unique_ptr<Item>> items_;    //!< アイテムリスト
+};
+
+}
