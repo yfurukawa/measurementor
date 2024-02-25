76f37c3311946f7f5bacd953ac0838647f36c4ee
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-25 19:12:51 +0900

    Projectクラス用のテストコードの追加

---
+++ test/domain/ProjectTest.cpp
@@ -0,0 +1,78 @@
+#include <memory>
+#include <string>
+#include "ProjectTest.h"
+#include "../../src/domain/Project.h"
+#include "../../src/domain/Item.h"
+#include "../../src/domain/Sprint.h"
+#include "../../src/domain/Task.h"
+#include "../../src/domain/domainPrimitives/MeasurementPrimitives.h"
+
+namespace measurementor
+{
+    void ProjectTest::SetUp()
+    {
+    }
+
+    void ProjectTest::TearDown()
+    {
+        delete sut;
+    }
+
+    TEST_F(ProjectTest, createJson_noAssignedItem)
+    {
+        Id id(1);
+        Name name("Test Project");
+        ParentId parentId(10);
+         
+        sut = new Project( id, name, parentId );
+
+        std::string expected(R"({"id":1,"name":"Test Project","remainingStoryPoints":0, "inProgressStoryPoints":0})");
+
+        EXPECT_EQ( expected, sut->createJson() );
+ 
+    }
+/*
+    TEST_F(ProjectTest, createJson_OneItemAndTask)
+    {
+        // Task
+        Id taskId(11);
+        Name taskName("Test Task");
+        Author author("Test Author");
+        ItemId itemId(1);
+        EstimatedTime estimatedTime(5.5);
+        Assignee assignee("Test Assignee");
+        Status taskStatus("In Progress");
+        StatusCode taskStatusCode(2);
+        UpdatedAt updatedAt("2024-02-23T19:18:25+09:00");
+        std::shared_ptr<Task> task = std::make_shared<Task>( taskId, taskName, author, itemId, estimatedTime, assignee, taskStatus, taskStatusCode, updatedAt );
+
+        // Id itemId(1); Declare in Task
+        Name itemName("Test Item");
+        ProjectId itemProjectId(10);
+        Point storyPoint(3);
+        EstimatedTime totalEstimatedTime(5.5);
+        Status status("open");
+        StatusCode statusCode(1);
+        SprintId sprintId(12);
+ 
+        std::shared_ptr<Item> item = std::make_shared<Item>( itemId, itemName, itemProjectId, sprintId, storyPoint, status, statusCode );
+
+        item->addTask( task );
+
+        // Sprint
+        Id id(1);
+        Name name("Test Sprint");
+        ProjectId projectId(10);
+        StartDate startDate("2024-02-23T12:34:56+09:00");
+        EndDate endDate("2024-02-25T12:34:56+09:00");
+        sut = new Sprint( id, name, startDate, endDate );        
+
+        sut->addItem( item );
+
+        std::string expected(R"({"endDate":"2024-02-25T12:34:56+09:00","id":1,"item":[{"id":1,"name":"Test Item","projectId":10,"sprintId":12,"status":"open","statusCode":1,"storyPoint":3,"task":[{"assignee":"Test Assignee","author":"Test Author","estimatedTime":5.5,"id":11,"itemId":1,"name":"Test Task","status":"In Progress","statusCode":2,"updatedAt":"2024-02-23T19:18:25+09:00"}],"totalEstimatedTime":5.5}],"name":"Test Sprint","projectId":0,"remainingWorkTime":5.5,"startDate":"2024-02-23T12:34:56+09:00","status":"","totalStoryPoint":0})");
+
+        EXPECT_EQ( expected, sut->createJson() );
+ 
+    }
+*/
+}
---
+++ test/domain/ProjectTest.h
@@ -0,0 +1,41 @@
+/*!
+ @file      ProjectTest.h
+ @brief     Projectをテストする
+*/
+#pragma once
+
+// ---------------< include >----------------------------
+#include <gtest/gtest.h>
+
+// --------------< namespace >---------------------------
+namespace measurementor
+{
+
+// ---------< forward declaration >----------------------
+class Project;
+
+/*!
+ @file      ProjectTest.h
+ @brief     Projectをテストする
+*/
+class ProjectTest : public ::testing::Test
+{
+public:
+    /*!
+     @brief  デフォルトコンストラクタ
+    */
+    ProjectTest() = default;
+
+    /*!
+     @brief  デフォルトデストラクタ
+    */
+    virtual ~ProjectTest() = default;
+
+protected:
+    Project* sut;
+
+    void SetUp();
+    void TearDown();
+};
+
+}
