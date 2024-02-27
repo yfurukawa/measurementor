84bb51507cadd3c0f512151502f1cb1e670571b1
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-02-27 21:11:39 +0900

    SprintのJsonObject生成指示を追加した

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main( int argc, char* argv[] )
 	measurementor::PtsDataCollector collector;
 	collector.correctData();
 	collector.permanentProjectData();
+	collector.permanentSprintData();
 	return 0;
 }
