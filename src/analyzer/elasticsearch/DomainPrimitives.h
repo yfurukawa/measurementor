b9959886cffb4c03645283faa28bc2f9321100d8
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-03-07 18:39:01 +0900

    Version型を追加

--- src/analyzer/elasticsearch/DomainPrimitives.h
+++ src/analyzer/elasticsearch/DomainPrimitives.h
@@ -8,7 +8,6 @@
 #include <string>
 #include "StrongType.h"
 
-
 /*!
  @def       ApiKey
  @brief     Basic認証のキー文字列
@@ -18,3 +17,13 @@ template< typename T >
 using ApiKeyRaw =
     StrongType<T, struct ApiKeyTag, Comparable, Printable>;
 using ApiKey = ApiKeyRaw<std::string>;
+
+/*!
+ @def       Version
+ @brief     Elasticsearchのバージョン
+ @tparam    std::string
+*/
+template< typename T >
+using VersionRaw =
+    StrongType<T, struct VersionTag, Comparable, Printable>;
+using Version = VersionRaw<std::string>;
