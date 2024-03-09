3933be65c73eba9f2a3f007f0c2820a7c5395be0
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-03-09 10:30:29 +0900

    定義と実装に分ける

---
+++ lib/libnetworkinterface/NetworkIfHelper.h
@@ -0,0 +1,32 @@
+/*!
+ @file      NetworkIfHelper.hpp
+ @brief     Networkに関する補助的な関数を提供する
+ @attention
+*/
+#pragma once
+
+// ---------------< include >----------------------------
+#include <netdb.h>
+#include <sstream>
+#include <string>
+#include "Hostname.h"
+#include "clx/base64.h"
+
+/*!
+ @fn         resolveHostname
+ @brief      ホスト名の名前解決をしてIPアドレスを得る
+ @param[in]  hostname RFC952及びRFC1123に準拠したホスト名
+ @return     IPアドレス（v4）
+*/
+std::string resolveHostname(Hostname& hostname)
+{
+    auto host = gethostbyname( hostname.get().c_str() );
+    std::stringstream ss;
+
+    ss << std::to_string( (unsigned char)*(host->h_addr_list[0]) ) << "."
+    << std::to_string( (unsigned char)*(host->h_addr_list[0] + 1) ) << "."
+    << std::to_string( (unsigned char)*(host->h_addr_list[0] + 2) ) << "."
+    << std::to_string( (unsigned char)*(host->h_addr_list[0] + 3) );
+
+    return ss.str();
+}
