06afa4988cf2e7adb6438cb60fc72a9c65081408
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-03-18 21:17:16 +0900

    IPv4クラスに比較演算子を定義した

--- lib/libnetworkinterface/IPv4.h
+++ lib/libnetworkinterface/IPv4.h
@@ -43,6 +43,38 @@ public:
     */
     std::string get();
 
+        /*!
+    @brief      同じ型の変数同士で等価（＝）演算を行う
+    @return     true : 左辺と右辺が等価
+    */
+    friend bool operator==( IPv4& lhs, IPv4& rhs ) {
+        return lhs.get() == rhs.get();
+    }
+
+    /*!
+    @brief      数値との間で等価（＝）演算を行う
+    @return     true : 左辺と右辺が等価
+    */
+    friend bool operator==( IPv4& lhs, std::string const& factor ) {
+        return lhs.get() == factor;
+    }
+
+    /*!
+    @brief      同じ型の変数同士で不等価（＜）演算を行う
+    @return     true : 左辺と右辺が不等価
+    */
+    friend bool operator!=( IPv4& lhs, IPv4& rhs ) {
+        return lhs.get() != rhs.get();
+    }
+
+    /*!
+    @brief      数値との間で不等価（＜）演算を行う
+    @return     true : 左辺と右辺が不等価
+    */
+    friend bool operator!=( IPv4& lhs, std::string const& factor ) {
+        return lhs.get() != factor;
+    }
+
 private:
     IPv4Raw<std::string> ipAddress_;       //!< サーバIPv4アドレス
 };
