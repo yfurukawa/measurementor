8eb3cb9517bb12225efc2d0c9b91433f9a9d5961
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-03-24 00:03:17 +0900

    書き方を整形した

--- lib/libclock/test/ChronosTest.cpp
+++ lib/libclock/test/ChronosTest.cpp
@@ -5,86 +5,86 @@
 
 void ChronosTest::SetUp()
 {
-    sut = new Chronos();
+  sut = new Chronos();
 }
 
 void ChronosTest::TearDown()
 {
-    delete sut;
+  delete sut;
 }
 
 TEST_F(ChronosTest, convertLocalToGMT_sameDay)
 {
-    ISO8601String localTime("2024-03-12T12:34:56+09:00");
-    ISO8601String expected("2024-03-12T03:34:56Z");
-    EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
+  ISO8601String localTime("2024-03-12T12:34:56+09:00");
+  ISO8601String expected("2024-03-12T03:34:56Z");
+  EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
 }
 
 TEST_F(ChronosTest, convertLocalToGMT_yesterday)
 {
-    ISO8601String localTime("2024-03-12T02:34:56+09:00");
-    ISO8601String expected("2024-03-11T17:34:56Z");
-    EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
+  ISO8601String localTime("2024-03-12T02:34:56+09:00");
+  ISO8601String expected("2024-03-11T17:34:56Z");
+  EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
 }
 
 TEST_F(ChronosTest, convertLocalToGMT_sameDay_west)
 {
-    ISO8601String localTime("2024-03-12T12:34:56-10:00");
-    ISO8601String expected("2024-03-12T22:34:56Z");
-    EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
+  ISO8601String localTime("2024-03-12T12:34:56-10:00");
+  ISO8601String expected("2024-03-12T22:34:56Z");
+  EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
 }
 
 TEST_F(ChronosTest, convertLocalToGMT_tomorrow_west)
 {
-    ISO8601String localTime("2024-03-12T22:34:56-10:00");
-    ISO8601String expected("2024-03-13T08:34:56Z");
-    EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
+  ISO8601String localTime("2024-03-12T22:34:56-10:00");
+  ISO8601String expected("2024-03-13T08:34:56Z");
+  EXPECT_EQ(expected.get(), sut->convertLocalToGMT(localTime).get());
 }
 
 TEST_F(ChronosTest, BasicString_GMT)
 {
-    ISO8601String time("20240219T121212Z");
-    EXPECT_EQ(1708312332, sut->convertToTime_t(time) );
+  ISO8601String time("20240219T121212Z");
+  EXPECT_EQ(1708312332, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, BasicString_JST)
 {
-    ISO8601String time("20240219T121212+0900");
-    EXPECT_EQ(1708279932, sut->convertToTime_t(time) );
+  ISO8601String time("20240219T121212+0900");
+  EXPECT_EQ(1708279932, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, BasicString_Other)
 {
-    ISO8601String time("20240219T121212-0900");
-    EXPECT_EQ(1708344732, sut->convertToTime_t(time) );
+  ISO8601String time("20240219T121212-0900");
+  EXPECT_EQ(1708344732, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, BasicString_microsec)
 {
-    ISO8601String time("20240219T121212.123456+0900");
-    EXPECT_EQ(1708279933, sut->convertToTime_t(time) );
+  ISO8601String time("20240219T121212.123456+0900");
+  EXPECT_EQ(1708279933, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, ExtendedString_GMT)
 {
-    ISO8601String time("2024-02-19T12:12:12Z");
-    EXPECT_EQ(1708312332, sut->convertToTime_t(time) );
+  ISO8601String time("2024-02-19T12:12:12Z");
+  EXPECT_EQ(1708312332, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, ExtendedString_JST)
 {
-    ISO8601String time("2024-02-19T12:12:12+09:00");
-    EXPECT_EQ(1708279932, sut->convertToTime_t(time) );
+  ISO8601String time("2024-02-19T12:12:12+09:00");
+  EXPECT_EQ(1708279932, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, ExtendedString_Other)
 {
-    ISO8601String time("2024-02-19T12:12:12-09:00");
-    EXPECT_EQ(1708344732, sut->convertToTime_t(time) );
+  ISO8601String time("2024-02-19T12:12:12-09:00");
+  EXPECT_EQ(1708344732, sut->convertToTime_t(time));
 }
 
 TEST_F(ChronosTest, ExtendedString_microsec)
 {
-    ISO8601String time("2024-02-19T12:12:12.543211+09:00");
-    EXPECT_EQ(1708279933, sut->convertToTime_t(time) );
+  ISO8601String time("2024-02-19T12:12:12.543211+09:00");
+  EXPECT_EQ(1708279933, sut->convertToTime_t(time));
 }
--- lib/libclock/test/ISO8601StringTest.cpp
+++ lib/libclock/test/ISO8601StringTest.cpp
@@ -5,68 +5,68 @@
 
 void ISO8601StringTest::SetUp()
 {
-    sut = nullptr;
+  sut = nullptr;
 }
 
 void ISO8601StringTest::TearDown()
 {
-    if (sut != nullptr)
-    {
-        delete sut;
-    }
+  if (sut != nullptr)
+  {
+    delete sut;
+  }
 }
 
 TEST_F(ISO8601StringTest, checkFormat_ShortLength)
 {
-    EXPECT_THROW( new ISO8601String("20240219T121212"), std::invalid_argument );
+  EXPECT_THROW(new ISO8601String("20240219T121212"), std::invalid_argument);
 }
 
 TEST_F(ISO8601StringTest, checkFormat_LongLength)
 {
-    EXPECT_THROW( new ISO8601String("2024-02-19T12:12:12.1234567+09:00"), std::invalid_argument );
+  EXPECT_THROW(new ISO8601String("2024-02-19T12:12:12.1234567+09:00"), std::invalid_argument);
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeDelimiterInCorrect_Basic)
 {
-    EXPECT_THROW( new ISO8601String("2024219T121212.1234567+0900"), std::invalid_argument );
+  EXPECT_THROW(new ISO8601String("2024219T121212.1234567+0900"), std::invalid_argument);
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeDelimiterInCorrect_Extended)
 {
-    EXPECT_THROW( new ISO8601String("2024-2-19T12:12:12+09:00"), std::invalid_argument );
+  EXPECT_THROW(new ISO8601String("2024-2-19T12:12:12+09:00"), std::invalid_argument);
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeFormatInCorrect_Basic_GMT)
 {
-    std::string expected("20240219T121212.123456Z");
-    sut = new ISO8601String(expected);
-    EXPECT_EQ( expected, sut->get() );
+  std::string expected("20240219T121212.123456Z");
+  sut = new ISO8601String(expected);
+  EXPECT_EQ(expected, sut->get());
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeFormatInCorrect_Basic_JST)
 {
-    std::string expected("20240219T121212.123456+0900");
-    sut = new ISO8601String(expected);
-    EXPECT_EQ( expected, sut->get() );
+  std::string expected("20240219T121212.123456+0900");
+  sut = new ISO8601String(expected);
+  EXPECT_EQ(expected, sut->get());
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeFormatInCorrect2_Basic_JST)
 {
-    std::string expected("20240219T121212+0900");
-    sut = new ISO8601String(expected);
-    EXPECT_EQ( expected, sut->get() );
+  std::string expected("20240219T121212+0900");
+  sut = new ISO8601String(expected);
+  EXPECT_EQ(expected, sut->get());
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeFormatInCorrect_Basic_ET)
 {
-    std::string expected("20240219T121212.123456-0900");
-    sut = new ISO8601String(expected);
-    EXPECT_EQ( expected, sut->get() );
+  std::string expected("20240219T121212.123456-0900");
+  sut = new ISO8601String(expected);
+  EXPECT_EQ(expected, sut->get());
 }
 
 TEST_F(ISO8601StringTest, checkFormat_DateAndTimeFormatInCorrect_Extended_GMT)
 {
-    std::string expected("2024-02-19T12:12:12.123456Z");
-    sut = new ISO8601String(expected);
-    EXPECT_EQ( expected, sut->get() );
+  std::string expected("2024-02-19T12:12:12.123456Z");
+  sut = new ISO8601String(expected);
+  EXPECT_EQ(expected, sut->get());
 }
