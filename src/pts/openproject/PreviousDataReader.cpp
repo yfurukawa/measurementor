483831779541768a1100f5ccf060d8111a857baf
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-03-24 15:46:55 +0900

    前回値のファイルを読み込むクラスの追加

---
+++ src/pts/openproject/PreviousDataReader.cpp
@@ -0,0 +1,15 @@
+/*!
+ @file  PreviousDataReader.cpp
+ @copyright Copyright 2024 Yoshihiro Furukawa
+*/
+#include "PreviousDataReader.h"
+
+std::list<std::map<std::string, std::string>> PreviousDataReader::collectSprintInformation(const measurementor::ProjectId& projectId)
+{
+
+}
+
+void PreviousDataReader::readJsonObjectFromPreviousData(std::filesystem::path previousFile)
+{
+
+}
---
+++ src/pts/openproject/PreviousDataReader.h
@@ -0,0 +1,63 @@
+/*!
+ @file      PreviousDataReader.h
+ @brief     OpenProjectクラスが生成したPreviousTask_x.jsonファイルを読み込み、メトリックス取得に必要なデータを抽出する
+ @copyright Copyright 2024 Yoshihiro Furukawa
+*/
+#pragma once
+
+// ---------------< include >----------------------------
+#include <filesystem>
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include "DomainPrimitivesForOpenProject.h"
+#include "ITextFileReader.h"
+#include "../../domain/domainPrimitives/MeasurementPrimitives.h"
+
+// ---------< forward declaration (Global) >-------------
+
+// --------------< namespace >---------------------------
+namespace pts
+{
+
+// ---------< forward declaration >----------------------
+class JsonParser;
+
+/*!
+ @class     PreviousDataReader
+ @brief     OpenProjectクラスが生成したPreviousTask_x.jsonファイルを読み込み、メトリックス取得に必要なデータを抽出する
+*/
+class PreviousDataReader final
+{
+public:
+  /*!
+   @brief  デフォルトコンストラクタ
+  */
+  PreviousDataReader() = delete;
+
+  /*!
+   @brief  デフォルトデストラクタ
+  */
+  virtual ~PreviousDataReader() = default;
+
+  /*!
+   @brief          指定されたプロジェクトに定義されているSprint情報を収集する
+   @param[in]      project 収集したいプロジェクト
+   @return         スプリントのリスト（Sprintの情報マップ（情報名、値）をリスト化したもの）
+  */
+  std::list<std::map<std::string, std::string>> collectSprintInformation(const measurementor::ProjectId& projectId);
+
+private:
+  std::unique_ptr<::ITextFileReader>
+    previousDataReader_;  //!< OpenProjectのサーバから取得したJsonオブジェクトをファイルに保存しておくためのWriter
+  std::unique_ptr<JsonParser> jsonParser_;  //!< Jsonオブジェクトのパーサ
+
+  /*!
+   @brief      保存されているJsonオブジェクトを読み出す
+   @param[in]  previousFaile  読み込み元のファイル名
+  */
+  void readJsonObjectFromPreviousData(std::filesystem::path previousFile);
+};
+
+}  // namespace pts
