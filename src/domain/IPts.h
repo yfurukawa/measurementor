3eba65442dbaedf525754672aa4c1972995a0cd0
Yoshihiro <yo4.furukawa@gmail.com>
2024-04-14 18:49:58 +0900

    関数コメントの内容を充実させた

--- src/domain/IPts.h
+++ src/domain/IPts.h
@@ -38,28 +38,67 @@ public:
 
   /*!
    @brief   全てのアクティブプロジェクトを収集する
-   @return  アクティブプロジェクトのリスト（Projectの情報マップ（情報名、値）をリスト化したもの）
+   @note    本メソッドは、PTS（Project Tracking System）で管理されているプロジェクトの内、
+            現在アクティブなものを収集する。また、プロジェクトは、複数の子プロジェクトを有する可能性がある。
+            親プロジェクトは、子プロジェクトのみを有し、スプリント等の管理データは末端のプロジェクトのみが有すると想定している。
+   @return  アクティブプロジェクトのリスト（Projectの情報マップ（情報名、値）をリスト化したもの）<br>
+            マップの詳細は、<br>
+            キー         ｜ 値<br>
+            projectId   ｜ PTSで付番されたプロジェクト識別番号<br>
+            projectName ｜ プロジェクトの名称<br>
+            parentId    ｜ （入れ子になっている場合の）親プロジェクトの識別番号
   */
   virtual std::list<std::map<std::string, std::string>> collectAllActiveProject() = 0;
 
   /*!
-   @brief          指定されたプロジェクトに定義されているSprint情報を収集する
-   @param[in]      project 収集したいプロジェクト
-   @return         スプリントのリスト（Sprintの情報マップ（情報名、値）をリスト化したもの）
+   @brief       指定されたプロジェクトに定義されているSprint情報を収集する
+   @param[in]   project 収集したいSprintを有するプロジェクトの識別番号
+   @return      スプリントのリスト（Sprintの情報マップ（情報名、値）をリスト化したもの）<br>
+                マップの詳細は、<br>
+                キー ｜ 値<br>
+                sprintId ｜ Sprintの識別番号<br>
+                projectId ｜ このSprintを持つプロジェクトの識別番号<br>
+                sprintName ｜ Sprintの名称<br>
+                status ｜ Sprintの状態（Open／Close）を表す<br>
+                startDate ｜ Sprint期間の開始日<br>
+                endDate ｜ Sprint期間の終了日<br>
   */
   virtual std::list<std::map<std::string, std::string>> collectSprintInformation(const measurementor::ProjectId& projectId) = 0;
 
   /*!
-   @brief          指定されたプロジェクトに定義されているItemの情報を収集する
-   @param[in]      project 収集したいプロジェクト
-   @return         プロダクトバックログアイテムのリスト（PBIの情報マップ（情報名、値）をリスト化したもの）
+   @brief       指定されたプロジェクトに定義されているItemの情報を収集する
+   @param[in]   project 収集したいItemを有するプロジェクトの識別番号
+   @return      プロダクトバックログアイテムのリスト（PBIの情報マップ（情報名、値）をリスト化したもの）<br>
+                マップの詳細は、<br>
+                キー ｜ 値<br>
+                itemId ｜ このItem（プラダクトバックログアイテム）の識別番号<br>
+                itemName ｜ Itemの名称（内容）
+                projectId ｜ このItemを持つプロジェクトの識別番号<br>
+                sprintId ｜ このItemが割り当てられたSprintの識別番号。プロダクトバックログに積まれている（Sprintに未割り当て）場合は、0になる。
+                storyPoint ｜ 見積もられたポイント数
+                status ｜ Itemの状態（Open／Close）<br>
+                statusCode ｜ Itemの状態の識別番号（Open： ／Close： ）<br>
+                totalEstimatedTime ｜ このItemが有する未完了タスクの総見積時間[H]。結果的に残作業時間となる。
   */
   virtual std::list<std::map<std::string, std::string>> collectItemInformation(const measurementor::ProjectId& projectId) = 0;
 
   /*!
-   @brief          指定されたプロジェクトに定義されているTaskの情報を収集する
-   @param[in]      project 収集したいプロジェクト
-   @return         タスクのリスト（タスクの情報マップ（情報名、値）をリスト化したもの）
+   @brief       指定されたプロジェクトに定義されているTaskの情報を収集する
+   @param[in]   project 収集したいプロジェクト
+   @return      タスクのリスト（タスクの情報マップ（情報名、値）をリスト化したもの）<br>
+                マップの詳細は、<br>
+                キー ｜ 値<br>
+                taskId ｜ タスクの識別番号<br>
+                taskName ｜ タスクの名称（内容）<br>
+                itemId ｜ このTaskを持つItemの識別番号<br>
+                projectId ｜ このTeskを持つプロジェクトの識別番号<br>
+                sprintId ｜ このTaskを持つItemが割り当てられたSprintの識別番号。<br>
+                author ｜ このTaskを出したメンバ名<br>
+                estimatedTime ｜ このタスクの見積時間[H]<br>
+                assignee ｜ Taskの担当者<br>
+                status ｜ Taskの状態（New、In Progress、Review、Close）<br>
+                statusCode ｜ Taskの状態の識別番号（New ： 1、In Progress ： 7、Review ： 15、Close ： 12）<br>
+                updatedAt ｜ Taskが更新された日時（ISO8601形式のローカルタイム）
   */
   virtual std::list<std::map<std::string, std::string>> collectTaskInformation(const measurementor::ProjectId& projectId) = 0;
 
