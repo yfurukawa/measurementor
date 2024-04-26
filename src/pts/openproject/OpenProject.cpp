199b06da5bae47cf1d156fb7c84854b4e767a85b
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-04-26 22:56:45 +0900

    フィルタの設定が間違っていたのを修正するとともに対象のデータのみにするようフィルタを追加した

--- src/pts/openproject/OpenProject.cpp
+++ src/pts/openproject/OpenProject.cpp
@@ -52,9 +52,14 @@ std::list<std::map<std::string, std::string>> OpenProject::collectSprintInformat
 
 std::list<std::map<std::string, std::string>> OpenProject::collectItemInformation(const measurementor::ProjectId& projectId)
 {
+  // filter : [
+  //            {"type":{"operator":"=","values":["4"]}},
+  //            {"status":{"operator":"=","values":["1","7","15"]}}
+  //          ]
+  // Featureのみを取得するようにしている。
   // ページサイズは、OpenProjectの設定に依存する
   std::string message("GET /api/v3/projects/" + std::to_string(projectId.get()) +
-                      "/work_packages?pageSize=4000");
+                      "/work_packages?pageSize=4000&filters=%5b%7b%22type%22:%7b%22operator%22:%22=%22,%22values%22:%5b%224%22%5d%7d%7d,%7b%22status%22:%7b%22operator%22:%22=%22,%22values%22:%5b%221%22,%227%22,%2215%22%5d%7d%7d%5d");
 
   std::string receivedJson = sendQueryMessage(message);
 
@@ -67,11 +72,15 @@ std::list<std::map<std::string, std::string>> OpenProject::collectItemInformatio
 
 std::list<std::map<std::string, std::string>> OpenProject::collectTaskInformation(const measurementor::ProjectId& projectId)
 {
-  // filter : [{"status":{"operator":"*","values":["*"]}}]
-  // 本フィルタは、フィルタの設定方法を定義しておくことが目的で、データのフィルタリングはしていない。
+  // filter : [
+  //            {"type":{"operator":"=","values":["1"]}},
+  //            {"status":{"operator":"=","values":["1","7","12","15"]}}
+  //          ]
+  // タスクの状態が、New、In Progress、Closed及びReviewのものを取得するようにしている。
+  // このフィルタがないと、statusがClosedのタスクは取得できない。
   // ページサイズは、OpenProjectの設定に依存する
   std::string message("GET /api/v3/projects/" + std::to_string(projectId.get()) +
-                      "/work_packages?pageSize=4000,filters=%5b%7b%22status%22:%7b%22operator%22:%22*%22,%22values%22:%5b%22*%22%5d%7d%7d%5d");
+                      "/work_packages?pageSize=4000&filters=%5b%7b%22type%22:%7b%22operator%22:%22=%22,%22values%22:%5b%221%22%5d%7d%7d,%7b%22status%22:%7b%22operator%22:%22=%22,%22values%22:%5b%221%22,%227%22,%2212%22,%2215%22%5d%7d%7d%5d");
 
   std::string receivedJson = sendQueryMessage(message);
   // TODO(yfurukawa) ここでサーバからの応答が正常であることを確認する
