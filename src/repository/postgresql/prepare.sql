d71028b7ecd0da32f1b6910084be4566320cd701
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-05-26 14:58:34 +0900

    タスクの前回値を保存しておくテーブル定義の追加

--- src/repository/postgresql/prepare.sql
+++ src/repository/postgresql/prepare.sql
@@ -6,13 +6,29 @@ CREATE DATABASE project_metrics;
 DROP TABLE task_metrics;
 
 CREATE TABLE task_metrics (
-    taskid integer PRIMARY KEY,
-    metrics_data jsonb
+  taskid integer PRIMARY KEY,
+  metrics_data jsonb
 );
 
 DROP TABLE rework_times;
 
 CREATE TABLE rework_times (
-    taskid integer PRIMARY KEY,
-    reworkTimes integer
+  taskid integer PRIMARY KEY,
+  reworkTimes integer
+);
+
+CREATE TABLE previous_task (
+  taskid        integer PRIMARY KEY,
+  taskName      text,
+  projectId     integer,
+  projectName   text,
+  sprintId      integer,
+  sprintName    text,  
+  author        text,
+  itemId        integer,
+  estimatedTime real,
+  assignee      text,
+  satus         char(15),
+  statusCode    integer,
+  updatedAt     char(25)
 );
